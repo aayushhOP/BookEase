53cdb0fc4a75c0d6fa74cec525d4eb4a6f3ccf45
aayushhOP <aayushkmrk1441@gmail.com>
2026-03-23 22:16:23 +0530

    basic server setup and db integration

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDb from './configs/db.js';
+
+const app = express();
+const port = 3000;
+
+await connectDb()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//api routes
+app.get('/', (req,res) => res.send('Server is Live!'))
+
+app.listen(port, () => console.log(`server listening at http://localhost:${port}`));
