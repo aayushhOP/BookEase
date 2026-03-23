86fc84be5c325c1ee84340a6f3730854311d1803
aayushhOP <aayushkmrk1441@gmail.com>
2026-03-23 22:52:29 +0530

    integrated clerk and inngest

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDb from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ await connectDb()
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 //api routes
 app.get('/', (req,res) => res.send('Server is Live!'))
