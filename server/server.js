b319f5778e7141c27e213cc1bc75ce0f2d380cef
aayushhOP <aayushkmrk1441@gmail.com>
2026-03-24 11:19:19 +0530

    created inngest function to modify DB

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDb from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -16,5 +18,6 @@ app.use(clerkMiddleware())
 
 //api routes
 app.get('/', (req,res) => res.send('Server is Live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 app.listen(port, () => console.log(`server listening at http://localhost:${port}`));
