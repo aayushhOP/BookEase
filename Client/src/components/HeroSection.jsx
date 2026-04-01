e665229379360b7c9c11517f11334a9be6ea7476
aayushhOP <aayushkmrk1441@gmail.com>
2026-04-01 12:22:51 +0530

    fixed the background image

--- Client/src/components/HeroSection.jsx
+++ Client/src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
 
   const navigate = useNavigate()
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("dhurandhar.jpeg")] bg-cover bg-center h-screen'>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/dhurandhar.jpeg")] bg-cover bg-center h-screen'>
       <img src={assets.JioStudios} alt='' className='max-h-11 lg:h-11 mt-20'/>
 
       <h1 className='text-5xl md:text-[65px] md:leading-18 font-semibold max-w-110'>Dhurandhar : <br /> The Revenge</h1>
